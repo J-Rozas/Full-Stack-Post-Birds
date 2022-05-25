e18bfbd93c929f515c7906f228a4a9dd09976fb9
Emilio J. R. Lima <jrozaslima@gmail.com>
2022-05-25 06:29:31 +0100

    Add route to delete a bird from the database

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -29,6 +29,19 @@ const createRouter = function (collection) {
       });
   });
 
+  router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    collection
+      .deleteOne({_id : ObjectID(id)})
+      .then(docs => res.json(docs))
+      .catch(err => {
+        console.error(err);
+        res.status(500);
+        res.json({status: 500, error: err});
+      });
+  });
+
   return router;
 };
 
